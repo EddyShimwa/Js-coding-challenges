e5cf3e20cff8a78dce8bfb94c748783b5aa1deb6
EddyShimwa <shedpray16@gmail.com>
2024-02-28 18:11:43 +0200

    solve the palindrome check

---
+++ Palindrome.js
@@ -0,0 +1,20 @@
+// Write a function that takes a string and returns true/false based on whether it is a palindrome or not (you can use a dictionary to know what a palyndrom is).
+
+const palindromeCheck = (str) => {
+  let isPalindrome = false;
+
+  let splitedString = str.split("").join("");
+
+  let reversedString = str.split("").reverse().join("");
+
+  if (splitedString === reversedString) {
+    isPalindrome = true;
+  }
+
+  console.log(isPalindrome);
+  return isPalindrome;
+};
+
+palindromeCheck("madam");
+
+module.exports = palindromeCheck;
